be8657592fb5bcbfa831ee853072fa9b0f8e9eb9
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-27 00:11:28 +0530

    new

--- hackerRank/Hamiltonian and Lagrangian.cpp
+++ hackerRank/Hamiltonian and Lagrangian.cpp
@@ -1,18 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(int argc, char const *argv[])
 {
     int n;
     cin>>n;
+    vector <int> a;
     for(int i = 0; i < n; i++)
     {
-        for(int j = i; j < n; j++)
-        {
-            
-        }
-        
+        int temp; cin>>temp;
+        a.push_back(temp);
     }
+    for(int i = 0; i < n; i++)
+    {
+        if(*max_element(a.begin()+i,a.end())==a[i])
+        {
+            cout<<a[i]<<" ";
+        }
+    }
+    
     
     return 0;
 }
