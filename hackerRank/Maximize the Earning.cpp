0065b929056abac4040aaf9ceedfe2b76b6a774d
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-26 19:35:06 +0530

    n

--- hackerRank/Maximize the Earning.cpp
+++ hackerRank/Maximize the Earning.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int t;
+    int t; cin>>t;
+    for(int i = 0; i < t; i++){
+        int n, k; cin>>n>>k;
+        vector <int> a;
+        for(int j = 0; j < n; j++)
+        {
+            int tem; cin>>tem;
+            a.push_back(tem);
+        }
+        int cnt = 0, max = 0;
+        
+    }
     
     return 0;
 }
