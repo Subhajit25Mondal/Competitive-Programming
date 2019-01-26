92b6e6164eb74d790b8597f5bbbcf38da3fe6dee
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-27 00:04:34 +0530

    nw

--- hackerRank/Hamiltonian and Lagrangian.cpp
+++ hackerRank/Hamiltonian and Lagrangian.cpp
@@ -7,6 +7,10 @@ int main(int argc, char const *argv[])
     cin>>n;
     for(int i = 0; i < n; i++)
     {
+        for(int j = i; j < n; j++)
+        {
+            
+        }
         
     }
     
