54272f6ec0b84bcc348ce9a04b951433831a3aa0
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-29 18:31:11 +0530

    nw

---
+++ hackerRank/SnackDown Contest.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h> 
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int t;
+    vector<int>::iterator it; 
+    cin>>t;
+    for(int i = 0; i < t; i++)
+    {
+        int pr; cin>>pr;
+        int p; cin>>p;
+        vector <int> a;
+        for(int j = 0; j < p; j++)
+        {
+            int temp; cin>>temp;
+            a.push_back(temp);
+        }
+        int q; cin>>q;
+        for(int j = 0; j < q; j++)
+        {
+            int temp; cin>>temp;
+            a.push_back(temp);
+        }
+
+        for
+        
+        
+    }
+    
+    return 0;
+}
