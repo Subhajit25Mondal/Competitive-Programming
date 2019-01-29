9ae37912750d4d40fa79d500c4ab66929e601df5
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-30 01:21:11 +0530

    nw

--- hackerRank/abc.cpp
+++ hackerRank/abc.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 int main(int argc, char const *argv[])
 {
     int n; cin>>n;
     vector <int> a;
+    vector <int> b;
     for(int i = 0; i < n; i++)
     {
         int temp; cin>>temp;
         a.push_back(temp);
+        b.push_back(temp);
     }
     
     int cnt=1, max = 0, max_n=0;
@@ -19,10 +22,7 @@ int main(int argc, char const *argv[])
 
     a.resize(std::distance(a.begin(), it)); 
 
-    for(int i = 0; i < a.size(); i++)
-    {
-        cout<<a[i]<<" ";
-    }
+    
 
     return 0;
 }
