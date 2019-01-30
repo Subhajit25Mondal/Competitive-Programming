7851f4c53d984196d75eecf811c9f2b8770fa51c
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-31 01:00:40 +0530

    nw

---
+++ Interview Preparation Kit (HackerRank)/Arrays: Left Rotation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n, r;
+    cin>>n>>r;
+    vector <int> a;
+    for(int i = 0; i < n; i++)
+    {
+        int temp; cin>>temp;
+        a.push_back(temp);
+    }
+    
+    return 0;
+}
