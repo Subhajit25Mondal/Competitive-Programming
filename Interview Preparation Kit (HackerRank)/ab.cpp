6b0bda36eb09373f5f7e4ea1fff894a827b8bcd4
Subhajit Mondal <subhajit25mondal@gmail.com>
2019-01-31 02:22:24 +0530

    nw

--- Interview Preparation Kit (HackerRank)/ab.cpp
+++ Interview Preparation Kit (HackerRank)/ab.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main(int argc, char const *argv[])
 {
+    int n;
+    cin>>n;
+    vector <int> a;
+    for(int i = 0; i < n; i++)
+    {
+        int temp; cin>>temp;
+        a.push_back(temp);
+    }
+
+    while(a.size()!=1){
+        
+    }
+    
     
     return 0;
 }
